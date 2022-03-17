5b18166df7cde0934bfa92ea5f38b7d580b06794
Johnny Villegas <johnny@leanware.io>
2022-03-16 23:25:21 -0500

    first commit

---
+++ assets/map/sea.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="sea" tilewidth="16" tileheight="16" tilecount="168" columns="12">
+ <image source="Water.png" width="192" height="224"/>
+ <tile id="8">
+  <animation>
+   <frame tileid="7" duration="350"/>
+   <frame tileid="8" duration="350"/>
+  </animation>
+ </tile>
+</tileset>
