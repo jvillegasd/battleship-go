9864884e0450aa71df651e32b3ca5c08385339da
= <=>
2022-03-21 16:11:19 -0500

    Button hover event handled

--- gui/button.py
+++ gui/button.py
@@ -13,14 +13,17 @@ class Button:
                  pos_y: float,
                  width: float,
                  height: float,
-                 btn_color: str = '#4C6785',
-                 text_color: str = '#4FA4B8') -> None:
+                 btn_color: str = '#475F77',
+                 btn_hover_color: str = '#D74B4B',
+                 text_color: str = '#FFFFFF') -> None:
         super().__init__()
 
         self.pressed = False
 
         self.top_rect = pygame.Rect(pos_x, pos_y, width, height)
         self.top_color = btn_color
+        self.current_top_color = btn_color
+        self.top_hover_color = btn_hover_color
 
         self.text_surf = GUI_FONT.render(text, True, text_color)
         self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
@@ -30,7 +33,7 @@ class Button:
           This method draws button on window. 
         """
 
-        pygame.draw.rect(window, self.top_color,
+        pygame.draw.rect(window, self.current_top_color,
                          self.top_rect, border_radius=12)
         window.blit(self.text_surf, self.text_rect)
 
@@ -44,10 +47,13 @@ class Button:
         action = False
         mouse_pos = pygame.mouse.get_pos()
         if self.top_rect.collidepoint(mouse_pos):
+            self.current_top_color = self.top_hover_color
             if pygame.mouse.get_pressed()[0] and not self.pressed:
                 action = True
                 self.pressed = True
             elif not pygame.mouse.get_pressed()[0]:
                 self.pressed = False
-
+        else:
+          self.current_top_color = self.top_color
+        
         return action
