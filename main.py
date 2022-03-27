a85b47b3f755b0b16d13f7c5e433c9d7392d15a8
= <=>
2022-03-27 18:27:48 -0500

    Code refactored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from typing import Tuple
+from typing import Tuple, List
 
 from gui.button import Button
 from sprites.rescue_ship import RescueShip
@@ -28,9 +28,16 @@ def create_gui_items() -> dict:
 
     start_button = Button(
         text='Start',
-        pos_x=155,
-        pos_y=400,
+        pos_x=655,
+        pos_y=200,
         width=70,
+        height=40
+    )
+    lock_ships = Button(
+        text='Lock ships',
+        pos_x=136,
+        pos_y=400,
+        width=110,
         height=40
     )
     grid = Grid(
@@ -39,8 +46,18 @@ def create_gui_items() -> dict:
     )
 
     gui_items = {
-        'start_button': start_button,
-        'map': grid
+        'start_button': {
+            'enabled': True,
+            'item': start_button
+        },
+        'lock_ships': {
+            'enabled': False,
+            'item': lock_ships
+        },
+        'map': {
+            'enabled': True,
+            'item': grid
+        }
     }
 
     return gui_items
@@ -88,15 +105,18 @@ def draw_window() -> None:
 
     # Draw GUI items
     for _, gui_item in GUI_ITEMS.items():
-        if type(gui_item) == list:
-            for item in gui_item:
+        if not gui_item['enabled']:
+            continue
+
+        if type(gui_item['item']) == list:
+            for item in gui_item['item']:
                 item.draw(WIN)
         else:
-            gui_item.draw(WIN)
+            gui_item['item'].draw(WIN)
 
     # Draw selected tile if grid is loaded
     if 'map' in GUI_ITEMS:
-        GUI_ITEMS['map'].draw_selected_tile(WIN)
+        GUI_ITEMS['map']['item'].draw_selected_tile(WIN)
 
     pygame.display.update()
 
@@ -111,8 +131,9 @@ def drag_and_drop_ship(
       This method handles necessary mouse events to drag and
       drop ships over grid.
     """
+
     global GUI_ITEMS
-    
+
     dragging = False
 
     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -123,8 +144,9 @@ def drag_and_drop_ship(
         if event.buttons[0]:
             # Remove rotation button when dragging
             dragging = True
-            GUI_ITEMS.pop('rotate_ship', None)
-            
+            if 'rotate_ship' in GUI_ITEMS:
+                GUI_ITEMS['rotate_ship']['enabled'] = False
+
             if 0 <= selected_ship < len(ships):
                 ships[selected_ship].move_ship(event.rel, grid, ships_rect)
                 ships_rect[selected_ship] = ships[selected_ship].rect
@@ -140,24 +162,55 @@ def drag_and_drop_ship(
 def enable_ship_rotation(
         grid: Grid,
         ships: list,
-        ships_rect: list,
+        ships_rect: List[pygame.Rect],
         selected_ship: int) -> None:
-
     """
       This method enables rotation button to selected ship.
     """
-    
+
+    global GUI_ITEMS
+
     # Add rotation button of selected ship to GUI
-    GUI_ITEMS['rotate_ship'] = ships[selected_ship].rotate_btn
-    
-    if ships[selected_ship].rotate_btn.click():
+    GUI_ITEMS['rotate_ship'] = {
+        'enabled': True,
+        'item': ships[selected_ship].rotate_btn
+    }
+
+    if handle_buttom_click(GUI_ITEMS['rotate_ship']):
         ships[selected_ship].rotate_ship(grid, ships_rect)
         ships_rect[selected_ship] = ships[selected_ship].rect
 
 
+def handle_buttom_click(gui_btn: dict) -> bool:
+    return gui_btn['enabled'] and gui_btn['item'].click()
+
+
+def ship_location_stage_events(
+        event,
+        ships: list,
+        ships_rect: List[pygame.Rect],
+        selected_ship: int) -> int:
+    """
+      This method handles pygame events related to ship location stage.
+    """
+
+    selected_ship, dragging = drag_and_drop_ship(
+        event, GUI_ITEMS['map']['item'], ships, ships_rect, selected_ship)
+
+    # Check if selected ship rotation buttom can be drawed
+    if 0 <= selected_ship < len(ships):
+        if not dragging:
+            enable_ship_rotation(
+                GUI_ITEMS['map']['item'], ships, ships_rect, selected_ship)
+    elif 'rotate_ship' in GUI_ITEMS:
+        GUI_ITEMS['rotate_ship']['enabled'] = False
+
+    return selected_ship
+
+
 def main():
     global GUI_ITEMS
-    
+
     # TODO: Create button to lock positions and create 2d arrays to simulate game (mine and enemry)
     # TODO: Create attack system and animations
     # TODO: Create UI (For maps: Create tab system, chat)
@@ -165,6 +218,7 @@ def main():
 
     # Handle game state variables
     game_started = False
+    ships_locked = False
     selected_ship = -1
 
     # Create ships and handle selected
@@ -182,20 +236,25 @@ def main():
                 run = False
 
             if game_started:
-                selected_ship, dragging = drag_and_drop_ship(
-                    event, GUI_ITEMS['map'], ships, ships_rect, selected_ship)
+                # Ships location stage
+                if not ships_locked:
+                    selected_ship = ship_location_stage_events(
+                        event, ships, ships_rect, selected_ship)
 
-                if 0 <= selected_ship < len(ships):
-                    if not dragging:
-                        enable_ship_rotation(
-                            GUI_ITEMS['map'], ships, ships_rect, selected_ship)
-                else:
-                  GUI_ITEMS.pop('rotate_ship', None)
-
-        if GUI_ITEMS['start_button'].click():
-            if not game_started:
+        if game_started:
+            if not ships_locked:
+                if handle_buttom_click(GUI_ITEMS['lock_ships']):
+                    ships_locked = True
+                    GUI_ITEMS['lock_ships']['enabled'] = False
+        else:
+            if handle_buttom_click(GUI_ITEMS['start_button']):
                 game_started = True
-                GUI_ITEMS['ships'] = ships
+                GUI_ITEMS['ships'] = {
+                    'enabled': True,
+                    'item': ships
+                }
+                GUI_ITEMS['lock_ships']['enabled'] = True
+                GUI_ITEMS['start_button']['enabled'] = False
 
         draw_window()
 
