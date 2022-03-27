f96179615333ae44473c245e2a20870a296ebeec
= <=>
2022-03-26 21:48:05 -0500

    Ship object with common logic created

--- gui/grid.py
+++ gui/grid.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 from typing import Tuple
+from sprites.ship import Ship
 
 
 class Grid:
@@ -78,7 +79,7 @@ class Grid:
         position_with_offset = pygame.Vector2(position) - grid_offset
         return int(position_with_offset[0] // self.tile_size), int(position_with_offset[1] // self.tile_size)
 
-    def is_ship_inside(self, ship) -> bool:
+    def is_ship_inside(self, ship: Ship) -> bool:
         """
           This method checks if ship is completely inside in grid.
         """
--- sprites/battleship.py
+++ sprites/battleship.py
@@ -2,73 +2,12 @@ import os
 import pygame
 from typing import Tuple
 from gui.grid import Grid
+from sprites.ship import Ship
 
 
-class Battleship:
+class Battleship(Ship):
 
     def __init__(self, pos_x: float, pos_y: float) -> None:
-        self.image = pygame.image.load(os.path.join(
-            'assets', 'ships', 'battleship', 'batleship.png'))
-        self.life = None
-        
-        self.rect = self.image.get_rect()
-        self.rect.x = pos_x
-        self.rect.y = pos_y
-
-    def move_ship(self, delta: Tuple[float, float], grid: Grid) -> None:
-        """
-          This method moves ship by adding a delta to current
-          rect position. If final position lets ship outside
-          the grid, the ship is moved to inital position.
-        """
-        
-        self.rect.x += delta[0]
-        self.rect.y += delta[1]
-        
-        if not grid.is_ship_inside(self):
-            self.rect.x -= delta[0]
-            self.rect.y -= delta[1]
-    
-    def dragged_ship_position(self, grid: Grid) -> None:
-        """
-          This method calculates where to drop dragged ship in a valid 
-          position of the grid.
-
-          The main idea is to use ship.rect.center as a pivot in order to
-          locate dragged ship. This pivot is going to be translated into
-          grid space in order to get a valid position, them, this coordinates is
-          upscaled and centered at tile.
-        """
-
-        grid_offset = (grid.pos_x, grid.pos_y)
-        tile_center_offset = (int(grid.tile_size // 2),
-                              int(grid.tile_size // 2))
-
-        # Translate ship.rect.center position
-        x, y = grid.translate_position(self.rect.center)
-
-        # Take translated position and upscale it
-        upscaled_x = x * grid.tile_size
-        upscaled_y = y * grid.tile_size
-        position_without_offset = pygame.Vector2(
-            (upscaled_x, upscaled_y)) + grid_offset
-
-        # Add another offset in order to locate ship.rect.center at center of the tile
-        position_without_offset += tile_center_offset
-        self.rect.center = position_without_offset
-    
-    def draw(self, window: pygame.display) -> None:
-        """
-          This method draws ship on window.
-        """
-        window.blit(self.image, self.rect)
-
-    def draw_hitbox(self, window: pygame.display) -> None:
-        """
-          This method draws ship rect and its center point on window.
-        """
-        
-        width = self.image.get_width()
-        height = self.image.get_height()
-        pygame.draw.rect(window, (255,0,0), (self.rect.x, self.rect.y, width, height), 1, border_radius=1)
-        pygame.draw.circle(window, (255,0,0), self.rect.center, 4) 
+        image_path = os.path.join(
+            'assets', 'ships', 'battleship', 'batleship.png')
+        super().__init__(image_path, pos_x, pos_y)
