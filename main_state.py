f261eaea3ae991527315f637521a568e365e6427
= <=>
2022-04-03 19:57:41 -0500

    Intro stage done

---
+++ main_state.py
@@ -0,0 +1,45 @@
+import pygame
+from typing import Tuple, List
+
+from stages.intro import Intro
+
+WIDTH, HEIGHT = 940, 640
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Battleship')
+
+# Color name: Little Greene French Grey Pale
+BACKGROUND_COLOR = (231, 231, 219)
+
+FPS = 30
+
+
+class GameState:
+
+    def __init__(self) -> None:
+        self.state = 'intro'
+        self.intro_stage = Intro()
+    
+    def intro(self) -> None:
+        states = self.intro_stage.process_events()
+        self.intro_stage.draw(WIN)
+        
+        
+    
+    def state_manager(self) -> None:
+      if self.state == 'intro':
+          self.intro()
+      
+      
+      
+
+
+game_state = GameState()
+
+
+def main():
+  while True:
+    game_state.state_manager()
+
+
+if __name__ == '__main__':
+    main()
---
+++ stages/intro.py
@@ -0,0 +1,79 @@
+import pygame
+import sys
+
+from gui.button import Button
+
+
+class Intro:
+    """ This class manages Intro stage game state. """
+    
+    def __init__(self) -> None:
+        self.states = {
+            'game_started': False
+        }
+        self.gui_items = self.__load_gui_items()
+
+        # Color name: Little Greene French Grey Pale
+        self.background_color = (231, 231, 219)
+
+    def __load_gui_items(self) -> dict:
+        """
+          This function creates and loads gui items
+          used in stage.
+        """
+        
+        start_button = Button(
+            text='Start',
+            pos_x=655,
+            pos_y=200,
+            width=70,
+            height=40
+        )
+
+        gui_items = {
+            'start_button': {
+                'enabled': True,
+                'item': start_button
+            }
+        }
+
+        return gui_items
+
+    def handle_buttom_click(self, gui_btn: dict) -> bool:
+        """ This function handles button click event """
+        return gui_btn['enabled'] and gui_btn['item'].click()
+
+    def draw(self, window: pygame.display) -> None:
+        """ This function draws gui items on window. """
+        
+        # Draw background
+        window.fill(self.background_color)
+
+        # Draw GUI items
+        for _, gui_item in self.gui_items.items():
+            if not gui_item['enabled']:
+                continue
+
+            if type(gui_item['item']) == list:
+                for item in gui_item['item']:
+                    item.draw(window)
+            else:
+                gui_item['item'].draw(window)
+
+        pygame.display.update()
+
+    def process_events(self) -> None:
+        """
+          This function handles pygame events related
+          to current stage.
+        """
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        if self.handle_buttom_click(self.gui_items['start_button']):
+            self.states['game_started'] = True
+
+        return self.states
