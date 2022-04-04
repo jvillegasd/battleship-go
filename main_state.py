1e565708c3a2181d64d5f6b77e822f2d0dc45fed
= <=>
2022-04-03 21:44:21 -0500

    Battle stage started

--- main_state.py
+++ main_state.py
@@ -1,17 +1,16 @@
 import pygame
 from typing import Tuple, List
 
+# Import stages
 from stages.intro import Intro
+from stages.battle import Battle
 from stages.ship_location import ShipLocation
 
+
+FPS = 30
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Battleship')
-
-# Color name: Little Greene French Grey Pale
-BACKGROUND_COLOR = (231, 231, 219)
-
-FPS = 30
 
 
 class GameState:
@@ -20,33 +19,41 @@ class GameState:
         self.state = 'intro'
         self.intro_stage = Intro()
         self.ship_location_stage = ShipLocation()
-    
+        self.battle_stage = Battle()
+
     def intro(self) -> None:
         states = self.intro_stage.process_events()
         self.intro_stage.draw(WIN)
-        
+
         if states['game_started']:
             self.state = 'ship_location'
 
     def ship_location(self) -> None:
         states = self.ship_location_stage.process_events()
         self.ship_location_stage.draw(WIN)
-    
+
+        if states['ship_locked']:
+            self.state = 'battle'
+            map_widget, ships = self.ship_location_stage.get_maps_and_ships()
+            self.battle_stage.load_maps_and_ships(map_widget, ships)
+
+    def battle(self) -> None:
+        states = self.battle_stage.process_events()
+        self.battle_stage.draw(WIN)
+
     def state_manager(self) -> None:
-      if self.state == 'intro':
-          self.intro()
-      elif self.state == 'ship_location':
-          self.ship_location()
-      
-      
-
-
-game_state = GameState()
-
+        if self.state == 'intro':
+            self.intro()
+        elif self.state == 'ship_location':
+            self.ship_location()
+        elif self.state == 'battle':
+            self.battle()
 
 def main():
-  while True:
-    game_state.state_manager()
+    game_state = GameState()
+    
+    while True:
+        game_state.state_manager()
 
 
 if __name__ == '__main__':
--- stages/battle.py
+++ stages/battle.py
@@ -17,6 +17,35 @@ class Battle:
         }
 
         self.gui_items = self.__load_gui_items()
+        
+        # Color name: Little Greene French Grey Pale
+        self.background_color = (231, 231, 219)
+
+    def draw(self, window: pygame.display) -> None:
+        """ This function draws gui items on window. """
+
+        # Draw background
+        window.fill(self.background_color)
+
+        # Draw GUI items
+        for _, gui_item in self.gui_items.items():
+            if not gui_item['enabled']:
+                continue
+
+            if type(gui_item['item']) == list:
+                for item in gui_item['item']:
+                    item.draw(window)
+            else:
+                gui_item['item'].draw(window)
+
+        # Draw selected tile for current tab
+        if self.gui_items['tabs']['enabled']:
+            if self.map_widget.ally_map_selected:
+                self.map_widget.ally_map.draw_selected_tile(window)
+            else:
+                self.map_widget.enemy_map.draw_selected_tile(window)
+
+        pygame.display.update()
 
     def process_events(self) -> dict:
         """
@@ -28,18 +57,20 @@ class Battle:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
+
         return self.states
 
     def load_maps_and_ships(self, maps: MapWidget, ships: list) -> None:
         """ This function loads into GUI map widget and ships """
+
+        self.map_widget = maps
+        self.ships = ships
         
-        self.gui_items['tabs']['item'] = maps
+        self.gui_items['tabs']['item'] = self.map_widget
         self.gui_items['tabs']['enabled'] = True
-        
-        self.gui_items['ships']['item'] = ships
+
+        self.gui_items['ships']['item'] = self.ships
         self.gui_items['ships']['enabled'] = True
-        
 
     def __load_gui_items(self) -> dict:
         """
@@ -58,7 +89,7 @@ class Battle:
             },
             'ships': {
                 'enabled': False,
-                'item': []
+                'item': None
             },
             'tabs': {
                 'enabled': False,
