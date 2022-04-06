3a6bd8f88fafe6b8f4d0c04d78a982ac446300cf
= <=>
2022-04-06 18:18:08 -0500

    feat: :sparkles: Basic server implemented

--- networking/server.py
+++ networking/server.py
@@ -1,16 +1,16 @@
-# https://www.youtube.com/watch?v=_fx7FQ3SP0U&list=PLzMcBGfZo4-kR7Rh-7JCVDN8lm3Utumvq&index=1
-
 import sys
 import socket
 from threading import Thread
 
-# Change print to Logging
+# TODO: Change print to Logging
 
 CONN_LIMIT = 2
-server_ip = ''
+BUFFER_SIZE = 2048
+
+server_ip = '192.168.1.1'
 port = 5555
 
-server_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     server_socket.bind((server_ip, port))
@@ -21,13 +21,29 @@ except socket.error as error:
 server_socket.listen(CONN_LIMIT)
 print('Waiting for a connection, Server started')
 
+
 def threaded_client(conn):
-  pass
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(BUFFER_SIZE)
+            reply = data.decode('utf-8')
+
+            if not data:
+                print('Disconnected')
+                break
+            else:
+                print('Received', reply)
+                print('Sending', reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
 
 while True:
-  conn, address = server_socket.accept()
-  print('Connected to', address, type(conn))
-  
-  new_client_conn = Thread(target=threaded_client, args=(conn,))
-  new_client_conn.start()
-# https://www.youtube.com/watch?v=_whymdfq-R4&list=PLzMcBGfZo4-kR7Rh-7JCVDN8lm3Utumvq&index=2
+    conn, address = server_socket.accept()
+    print('Connected to', address, type(conn))
+
+    new_client_conn = Thread(target=threaded_client, args=(conn,))
+    new_client_conn.start()
