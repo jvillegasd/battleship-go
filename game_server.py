5cb93db4cc8ac15f81eece5f32440caaa75be1fb
= <=>
2022-04-14 20:59:14 -0500

    feat: :sparkles: Server is closed when started server window is closed

--- game_server.py
+++ game_server.py
@@ -1,17 +1,22 @@
 import tkinter as tk
+from types import TracebackType
+from typing import Optional, Type
 
 from networking.server import Server
 
 
-class GameServer(tk.Frame):
+class GameServerWindow(object):
     """ This class manages game server frame. """
 
-    def __init__(self, parent: tk.Tk, *args, **kwargs) -> None:
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        
+    def __init__(self) -> None:
+        self.parent = tk.Tk(className='Battleship - Game server')
+        self.parent.geometry('300x300')
+        self.parent.resizable(width=False, height=False)
+        self.parent.eval('tk::PlaceWindow . center')
+
         # Core attributes
         self.server = None
-        self.parent = parent
+        # self.parent = parent
         self.polling_interval = 1000
 
         # Top frame for start and stop game server
@@ -40,14 +45,35 @@ class GameServer(tk.Frame):
         self.text_display.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
         self.text_display.config(background='#F4F6F7',
                                  highlightbackground='grey', state='disabled')
-        self.client_frame.pack(side=tk.BOTTOM, pady=(0, 20))
+        self.client_frame.pack(side=tk.TOP, pady=(5, 0))
+
+        # Dev sign frame
+        self.dev_sign_frame = tk.Frame(self.parent)
+        self.lbl_dev_sign = tk.Label(
+            self.dev_sign_frame, text='Made by jvillegasd :D')
+        self.lbl_dev_sign.pack(side=tk.RIGHT)
+        self.dev_sign_frame.pack(side=tk.BOTTOM, pady=(0, 20))
 
         # Define a timer for client refresh polling
-        self.text_display.after(self.polling_interval, self.refresh_clients_display)
+        self.text_display.after(self.polling_interval,
+                                self.refresh_clients_display)
+
+    def __enter__(self) -> 'GameServerWindow':
+        return self
+
+    def __exit__(
+            self,
+            exctype: Optional[Type[BaseException]],
+            excinst: Optional[BaseException],
+            exctb: Optional[TracebackType]) -> None:
+        """ This build-in function close server if window is closed. """
+        if self.server:
+            self.server.stop_server()
+            self.server = None
 
     def start_server(self) -> None:
         """ This function starts a new server instance. """
-        
+
         self.start_btn.config(state=tk.DISABLED)
         self.stop_btn.config(state=tk.NORMAL)
 
@@ -62,7 +88,7 @@ class GameServer(tk.Frame):
 
     def stop_server(self) -> None:
         """ This function stops current server instances and delete it. """
-        
+
         self.start_btn.config(state=tk.NORMAL)
         self.stop_btn.config(state=tk.DISABLED)
 
@@ -74,28 +100,24 @@ class GameServer(tk.Frame):
 
     def refresh_clients_display(self) -> None:
         """ This function polls connected clients to current server. """
-        
+
         if self.server:
             connected_clients = self.server.get_connected_clients()
             self.text_display.config(state=tk.NORMAL)
             self.text_display.delete('1.0', tk.END)
-            
+
             for client_name in connected_clients:
                 self.text_display.insert(tk.END, client_name + '\n')
-            
+
             self.text_display.config(state=tk.DISABLED)
 
-        self.text_display.after(self.polling_interval, self.refresh_clients_display)
+        self.text_display.after(self.polling_interval,
+                                self.refresh_clients_display)
 
 
 def main() -> None:
-    root = tk.Tk(className='Battleship - Game server')
-    root.geometry('300x300')
-    root.resizable(width=False, height=False)
-    root.eval('tk::PlaceWindow . center')
-
-    GameServer(root).pack(side='top', fill='both', expand=True)
-    root.mainloop()
+    with GameServerWindow() as gs:
+        gs.parent.mainloop()
 
 
 if __name__ == '__main__':
